745c893e9b5d52b3d60ddfb138b6a4f59f098863
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-09-16 21:37:50 -0300

    Obtendo as inforamações de um cliente

--- src/main/java/io/github/walisonfbalves/clientes/rest/ClienteController.java
+++ src/main/java/io/github/walisonfbalves/clientes/rest/ClienteController.java
@@ -5,6 +5,7 @@ import io.github.walisonfbalves.clientes.model.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/clientes")
@@ -19,7 +20,14 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente salvar( @RequestBody Cliente cliente) {
+    public Cliente salvar(@RequestBody Cliente cliente) {
         return repository.save(cliente);
+    }
+
+    @GetMapping("{id}")
+    public Cliente acharPorId(@PathVariable Integer id) {
+        return repository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 }
