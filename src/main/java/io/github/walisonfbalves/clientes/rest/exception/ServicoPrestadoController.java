a23bf04b30763d3a889330b4ec91cccbd76ccab7
walisonfbalves <walisonfbalves.ti@gmail.com>
2024-09-30 12:57:16 -0300

    Metodo de pesquisa de serviÃçÃos prestados

--- src/main/java/io/github/walisonfbalves/clientes/rest/exception/ServicoPrestadoController.java
+++ src/main/java/io/github/walisonfbalves/clientes/rest/exception/ServicoPrestadoController.java
@@ -13,6 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/servicos-prestados")
@@ -42,5 +43,13 @@ public class ServicoPrestadoController {
         servicoPrestado.setCliente(cliente);
         servicoPrestado.setValor(bigDecimalConverter.converter(dto.getPreco()));
         return servicoPrestadoRepository.save(servicoPrestado);
+    }
+
+    @GetMapping
+    public List<ServicoPrestado> pesquisar(
+            @RequestParam(value = "nome", required = false, defaultValue = "") String nome,
+            @RequestParam(value = "mes", required = false) Integer mes
+    ) {
+        return servicoPrestadoRepository.findByNomeClienteAndMes("%" + nome + "%" , mes);
     }
 }
